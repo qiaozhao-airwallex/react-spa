323ed4c9fc07a8fb4b0c005221543277b1d9a5c0
Qiao Zhao <qiao.zhao@neustar.biz>
2018-03-07 17:48:49 +1100

    Only check REACT_APP_BACK_END_URL property

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,5 +1,5 @@
 export const backendURL =
-    (process.env.NODE_ENV === 'production' && process.env.REACT_APP_BACK_END_URL != null) ?
+    (process.env.REACT_APP_BACK_END_URL != null) ?
         process.env.REACT_APP_BACK_END_URL :
         "http://localhost:8080/"
 export const imageUploadURL = backendURL + "image/"
