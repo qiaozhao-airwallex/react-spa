e8dbb2afe972ace3631e7fb10af5649008209591
Qiao Zhao <qiao.zhao@neustar.biz>
2018-03-07 17:15:07 +1100

    Use process.env to determine envrionment

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,4 +1,7 @@
-export const backendURL = "http://localhost:8080/"
+export const backendURL =
+    (process.env.NODE_ENV === 'production' && process.env.REACT_APP_BACK_END_URL != null) ?
+        process.env.REACT_APP_BACK_END_URL :
+        "http://localhost:8080/"
 export const imageUploadURL = backendURL + "image/"
 export const imageServeURL = backendURL + "uploaded/"
 export const productBackendURL = backendURL + "product"
